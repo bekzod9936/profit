0ccce27a311e053380fa53ec27c05fa3ef1caf5f
bekzod9936 <bekzodnorkulov9936@gmail.com>
2022-03-03 16:59:41 +0300

    test

--- src/pages/consulting/components/Success/style.js
+++ src/pages/consulting/components/Success/style.js
@@ -2,7 +2,16 @@ import { device } from 'utils/style'
 import styled from 'styled-components'
 import { ReactComponent as Success } from 'assets/icons/success.svg'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-grow: 1;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  grid-gap: 10px;
+`
 
 export const Title = styled.div`
   color: white;
