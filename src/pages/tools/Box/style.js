26fa9dcc06644c7d10630d10fb72ab02056dbd1b
bekzod9936 <bekzodnorkulov9936@gmail.com>
2022-03-05 15:10:09 +0300

    tools ready

--- src/pages/tools/Box/style.js
+++ src/pages/tools/Box/style.js
@@ -10,26 +10,44 @@ export const Container = styled(motion.div)`
   align-items: center;
   justify-content: center;
   box-shadow: 6px 6px 14px 0 rgba(0, 0, 0, 0.1), -3px -3px 18px 0 black;
-  backdrop-filter: blur(300px);
-  -webkit-backdrop-filter: blur(300px);
-  background: rgba(255, 255, 255, 0.01);
+  background: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0.1) -1.79%,
+    rgba(255, 255, 255, 0.024) 123.08%
+  );
+  backdrop-filter: blur(120px);
   @media (max-width: ${device.mobile}) {
-    height: 230px;
-    max-height: 230px;
-    min-height: 230px;
+    border-radius: 40px;
+
+    backdrop-filter: blur(171.148px);
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    backdrop-filter: blur(82.6722px);
+    border-radius: 20px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    border-radius: 25px;
+    backdrop-filter: blur(96.4509px);
   }
 `
 
 export const Title = styled.div`
   color: white;
   font-size: 25px;
+  line-height: 27px;
   max-width: 80%;
-  @media (min-width: ${device.mobile}) and (max-width: ${device.desktop}) {
-    font-size: 30px;
-  }
+  font-weight: 600;
   @media (max-width: ${device.mobile}) {
-    max-width: 100%;
-    font-size: 25px;
+    font-size: 35.6557px;
+    line-height: 39px;
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    font-size: 17.2234px;
+    line-height: 19px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    font-size: 20.0939px;
+    line-height: 22px;
   }
 `
 export const Img = styled.img`
@@ -38,6 +56,22 @@ export const Img = styled.img`
   top: -20px;
   width: 50px;
   height: 50px;
+
+  @media (max-width: ${device.mobile}) {
+    width: 70px;
+    height: 70px;
+    top: -30px;
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    width: 35px;
+    height: 35px;
+    top: -15px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    width: 40px;
+    height: 40px;
+    top: -10px;
+  }
 `
 export const containerVariants = {
   hover: {
--- src/pages/tools/style.js
+++ src/pages/tools/style.js
@@ -8,20 +8,37 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 70px 0 50px;
+
+  @media (max-width: ${device.mobile}) {
+    margin: 70px 0 30px;
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    margin: 40px 0 10px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    margin: 40px 0 20px;
+  }
 `
 
 const common = css`
   color: white;
-  font-size: 50px;
   text-align: center;
   font-family: 'GilroyBold' !important;
   font-weight: 800;
   line-height: 60px;
-  @media (min-width: ${device.mobile}) and (max-width: ${device.desktop}) {
-    font-size: 35px;
-  }
+  font-size: 50px;
+
   @media (max-width: ${device.mobile}) {
-    font-size: 22px;
+    font-size: 30px;
+    line-height: 47px;
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    font-size: 35px;
+    line-height: 55px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    font-size: 40px;
+    line-height: 60px;
   }
 `
 
@@ -42,16 +59,24 @@ export const Title2 = styled.div`
 export const Wrapper = styled.div`
   width: 90%;
   display: grid;
-  grid-template-columns: 300px 300px 300px;
-  grid-template-rows: 163px 163px;
-  grid-gap: 30px 35px;
+  grid-gap: 50px 35px;
   padding-top: 64px;
   justify-content: center;
-  @media (min-width: ${device.mobile}) and (max-width: ${device.desktop}) {
-    grid-template-columns: 40% 40%;
-    grid-gap: 25px;
-  }
+  grid-template-columns: repeat(3, minmax(auto, 300px));
+  grid-template-rows: repeat(2, minmax(auto, 163px));
   @media (max-width: ${device.mobile}) {
-    grid-template-columns: 100%;
+    grid-template-columns: minmax(auto, 420px);
+    grid-template-rows: repeat(6, minmax(auto, 230px));
+    grid-gap: 100px 0;
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    grid-template-columns: repeat(3, minmax(auto, 200px));
+    grid-template-rows: repeat(2, minmax(auto, 110px));
+    grid-gap: 30px 25px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    grid-template-columns: repeat(3, minmax(auto, 240px));
+    grid-template-rows: repeat(2, minmax(auto, 130px));
+    grid-gap: 35px 30px;
   }
 `
