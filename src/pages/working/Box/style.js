e910dea848b4b1dded8150128ea0b1b38020eca3
bekzod9936 <bekzodnorkulov9936@gmail.com>
2022-03-05 14:11:08 +0300

    working ready

--- src/pages/working/Box/style.js
+++ src/pages/working/Box/style.js
@@ -1,9 +1,10 @@
-import { device } from 'utils/style';
-import styled from 'styled-components';
+import { device } from 'utils/style'
+import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-`;
+  align-items: center;
+`
 
 export const Title = styled.div`
   color: white;
@@ -11,16 +12,21 @@ export const Title = styled.div`
   padding-left: 45px;
   line-height: 40px;
   font-weight: 600;
-  width: 80%;
-  @media (min-width: ${device.mobile}) and (max-width: ${device.desktop}) {
-    padding-left: 20px;
-    font-size: 25px;
-  }
   @media (max-width: ${device.mobile}) {
-    padding-left: 10px;
-    font-size: 18px;
+    padding-left: 30px;
   }
-`;
+
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    padding-left: 25px;
+    font-size: 16px;
+    line-height: 22px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    padding-left: 35px;
+    font-size: 22px;
+    line-height: 30px;
+  }
+`
 export const WrapIcon = styled.div`
   border: 1px solid white;
   display: flex;
@@ -29,10 +35,7 @@ export const WrapIcon = styled.div`
   border-radius: 50%;
   padding: 20px;
   height: fit-content;
-  @media (min-width: ${device.mobile}) and (max-width: ${device.desktop}) {
+  @media (max-width: ${device.desktop}) {
     padding: 10px;
   }
-  @media (max-width: ${device.mobile}) {
-    padding: 10px;
-  }
-`;
+`
--- src/pages/working/style.js
+++ src/pages/working/style.js
@@ -7,15 +7,70 @@ import { ReactComponent as Analyze } from 'assets/icons/analyze.svg'
 import { ReactComponent as Product } from 'assets/icons/product.svg'
 import { ReactComponent as Structure } from 'assets/icons/structure.svg'
 
-const icon = css`
-  @media (min-width: ${device.mobile}) and (max-width: ${device.desktop}) {
-    width: 35px;
-    height: 35px;
-  }
+export const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  height: fit-content;
+  flex-direction: column;
+`
+
+export const Wrapper = styled.div`
+  width: 90%;
+  display: grid;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: space-between;
+  border: 2px solid #57576b;
+  backdrop-filter: blur(120px);
+  grid-template-columns: 1fr 1fr;
+  -webkit-backdrop-filter: blur(120px);
+  background: rgba(255, 255, 255, 0.1);
+  box-shadow: 6px 6px 14px 0 rgba(255, 255, 255, 0.1),
+    -6px -6px 14px 0 rgba(255, 255, 255, 0.1);
+  border-radius: 30px;
+
+  grid-gap: 72px 120px;
+  padding: 55px 50px;
+  margin-top: 70px;
+  max-width: 1230px;
 
   @media (max-width: ${device.mobile}) {
+    grid-template-columns: 100%;
+    padding: 80px 20px;
+    margin-top: 40px;
+    grid-gap: 80px 0;
+    max-width: 480px;
+  }
+
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
+    margin-top: 54px;
+    padding: 30px;
+    max-width: 690px;
+    grid-gap: 40px 30px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    margin-top: 64px;
+    padding: 45px 30px;
+    max-width: 930px;
+    grid-gap: 50px 35px;
+  }
+`
+
+const icon = css`
+  width: 47px;
+  height: 47px;
+  @media (max-width: ${device.mobile}) {
+    width: 47px;
+    height: 47px;
+  }
+  @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
     width: 25px;
     height: 25px;
+  }
+  @media (min-width: ${device.planshet}) and (max-width: ${device.laptop}) {
+    width: 35px;
+    height: 35px;
   }
 `
 
@@ -36,39 +91,4 @@ export const IconLayOut = styled(LayOut)`
 `
 export const IconLaunch = styled(Launch)`
   ${icon}
-`
-
-export const Container = styled.div`
-  width: 100vw;
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  @media (max-width: ${device.mobile}) {
-    padding: 50px 0;
-  }
-`
-
-export const Wrapper = styled.div`
-  width: 90%;
-  display: grid;
-  flex-grow: 1;
-  grid-gap: 72px 120px;
-  padding: 50px;
-  border-radius: 30px;
-  margin-top: 70px;
-  align-items: center;
-  justify-content: center;
-  border: 2px solid #57576b;
-  backdrop-filter: blur(120px);
-  grid-template-columns: 1fr 1fr;
-  -webkit-backdrop-filter: blur(120px);
-  background: rgba(255, 255, 255, 0.1);
-  box-shadow: 6px 6px 14px 0 rgba(255, 255, 255, 0.1),
-    -6px -6px 14px 0 rgba(255, 255, 255, 0.1);
-  max-width: 1230px;
-  @media (max-width: ${device.mobile}) {
-    grid-template-columns: 100%;
-    padding: 15px;
-  }
 `
